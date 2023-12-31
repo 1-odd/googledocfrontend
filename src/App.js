bbc0442f689b466913f2f554f70073fbd98c3042
Ritik Kumar <ritikraj2018634@gmail.com>
2023-12-31 20:38:45 +0530

    frontend work completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Editor from './components/Editor';
 import './App.css';
+import {BrowserRouter as  Router , Routes,Route , Navigate} from 'react-router-dom';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   return (
-      <>
-      <Editor/>
-      </>
+      <Router>
+          <Routes>
+            <Route path='/' element = {<Navigate replace to = {`/doc/${uuid()}`}/> } />
+            < Route path='/doc/:id'element = { <Editor/>} /> 
+          </Routes>
+      </Router>
   );
 }
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,8 +1,10 @@
 import {Box} from '@mui/material';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
-import { useEffect } from 'react';
+import { useEffect ,useState} from 'react';
 import styled from '@emotion/styled';
+import {io} from 'socket.io-client';
+import { useParams } from 'react-router-dom';
 
 
 const Component = styled.div`
@@ -32,9 +34,94 @@ const toolbarOptions = [
 
 
 const Editor = ()=>{
-    useEffect(()=>{
+
+
+  const [socket,setSocket] = useState();
+  const [quill , setQuill] = useState();
+
+  const {id} = useParams();
+
+
+
+    useEffect(()=>{ // for editor 
        const quillServer =  new Quill('#container',{theme : 'snow',modules:{toolbar:toolbarOptions}})
+       quillServer.disable();  // server disable untill  exist doc load
+       quillServer.setText('Loading the document please wait for a moment ...')
+       setQuill(quillServer);
+    },[]);
+
+    useEffect (()=>{  // for socket connection
+
+      const socketServer = io('http://localhost:3022');  // create connetion
+
+      setSocket(socketServer);
+
+      return ()=>{
+        // socket.off();
+        socketServer.disconnect();    // close connection
+      }
+
     },[])
+
+    useEffect(()=>{  // detect realtime changes at own side and send to server
+
+        if(socket===null || quill === null)return;
+
+      const handleChange = (delta,oldData,source)=>{
+
+        if(source !== 'user')return;
+
+       socket && socket.emit('send-changes',delta);
+      }
+
+      quill && quill.on('text-change',handleChange);
+
+      return ()=>{
+       quill && quill.off('text-change',handleChange)
+      }
+
+      
+    },[quill,socket]);
+
+
+    useEffect(()=>{  // detect realtime changes from other users and update your docs
+
+      if(socket===null || quill === null)return;
+
+    const handleChange = (delta)=>{  // recived only changes so no need other variables
+
+     // update chanes with the help of quill
+     quill.updateContents(delta);
+
+     
+    }
+
+    socket && socket.on('receive-changes',handleChange);
+
+    return ()=>{
+      socket && socket.off('receive-changes',handleChange)
+    }
+
+    
+  },[quill,socket])
+
+
+  useEffect(()=>{    // apply some validation on changes based on id
+
+    if(socket===null || quill === null)return;
+
+    socket && socket.once('load-document',document =>{
+     quill && quill.setContents(document);
+      quill && quill.enable();
+    })
+
+    socket && socket.emit('get-document',id);
+
+
+  },[quill,socket,id]);
+
+
+
     return (
 
        <Component>
